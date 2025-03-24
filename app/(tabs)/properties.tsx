e5d311d056e6ecedfe7afc01329bcd9794ee356a
Eliuter Koka <ekoka56@gmail.com>
2025-03-24 06:27:48 +0300

    Add files via upload

---
+++ app/(tabs)/properties.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import Logo from "@/components/svg/Logo";
+
+export default function PropertiesScreen() {
+  return (
+    <View className="flex-1 items-center justify-center px-4 bg-background">
+      <View className="flex-col items-start gap-y-2 w-full">
+        <Logo width={40} height={40} />
+        <Text className="text-3xl text-left text-text-main font-accent">
+          Welcome, Properties
+        </Text>
+      </View>
+    </View>
+  );
+}
