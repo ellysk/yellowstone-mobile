72729e9e4263c69f640d9657e21f53fe6a22de8e
Eliuter Koka <ekoka56@gmail.com>
2025-05-11 18:04:10 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,6 @@
-import "../../global.css";
 import { View, Text } from "react-native";
-import * as Font from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState } from "react";
-import Logo from "@/components/svg/Logo";
-
-// Prevent the splash screen from auto-hiding
-SplashScreen.preventAutoHideAsync();
 
 export default function DashboardScreen() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  const loadFonts = async () => {
-    await Font.loadAsync({
-      Inter: require("../../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
-      Roboto: require("../../assets/fonts/Roboto-VariableFont_wdth,wght.ttf"),
-      "Space Grotesk": require("../../assets/fonts/SpaceGrotesk-VariableFont_wght.ttf"),
-    });
-    setFontsLoaded(true);
-    SplashScreen.hideAsync(); // Hide the splash screen once fonts are loaded
-  };
-
-  useEffect(() => {
-    loadFonts();
-  }, []);
-
-  if (!fontsLoaded) {
-    // Keep the splash screen visible while fonts are loading
-    return null;
-  }
-
   return (
     <View className="flex-1 items-center justify-center px-4 bg-background">
       <View className="flex-col items-start gap-y-2 w-full">
