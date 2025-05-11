9350bac0cfafa53a85fa917b11121a7cf3219341
Eliuter Koka <ekoka56@gmail.com>
2025-05-11 19:38:24 +0300

    Update tenants.tsx

--- app/(tabs)/tenants.tsx
+++ app/(tabs)/tenants.tsx
@@ -5,7 +5,6 @@ export default function TenantsScreen() {
   return (
     <View className="flex-1 items-center justify-center px-4 bg-background">
       <View className="flex-col items-start gap-y-2 w-full">
-        <Logo width={40} height={40} />
         <Text className="text-3xl text-left text-text-main font-accent">
           Welcome, Tenants
         </Text>
