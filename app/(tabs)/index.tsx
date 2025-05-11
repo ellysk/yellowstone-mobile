a2ce829f36b92aafb9dc65f0aad341d2f55bb5b9
Eliuter Koka <ekoka56@gmail.com>
2025-05-11 19:37:37 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ export default function DashboardScreen() {
   return (
     <View className="flex-1 items-center justify-center px-4 bg-background">
       <View className="flex-col items-start gap-y-2 w-full">
-        <Logo width={40} height={40} />
         <Text className="text-3xl text-left text-text-main font-accent">
           Welcome, Dashboard
         </Text>
