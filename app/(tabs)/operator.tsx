ab18f997aa416952aecc6a171df6691cb70b1cb8
Eliuter Koka <ekoka56@gmail.com>
2025-05-17 11:56:56 +0300

    Add OperatorScreen

--- app/(tabs)/operator.tsx
+++ app/(tabs)/operator.tsx
@@ -1,13 +1,40 @@
-import { View, Text } from "react-native";
+import InfoView, { InfoList } from "@/components/InfoView";
+import { ProfileView, ProfileViewProps } from "@/components/ProfileView";
+import { View } from "react-native";
 
 export default function OperatorScreen() {
   return (
-    <View className="flex-1 items-center justify-center px-4 bg-background">
-      <View className="flex-col items-start gap-y-2 w-full">
-        <Text className="text-3xl text-left text-text-main font-accent">
-          Welcome, Operator
-        </Text>
+    <View className="flex-1 flex-col gap-y-6 bg-background-main">
+      <ProfileView {...profileData} />
+      <View>
+        <InfoList
+          headerProps={{ header: "Allowed Tag Zones" }}
+          data={allowedTagZones}
+        />
       </View>
     </View>
   );
 }
+
+const profileData: ProfileViewProps = {
+  name: "Kayce Dutton",
+  company: "Yellowstone Ranch",
+  details: "A ranch from the show Yellowstone, since 1886",
+  profileImage:
+    "https://www.paramountshop.co.uk/cdn/shop/files/yellowstone-logo-sticker-assorted-pack-of-3-423554_1500x.png?v=1718295715",
+};
+
+const allowedTagZones = [
+  {
+    title: "North, District 1",
+  },
+  {
+    title: "East, District 2",
+  },
+  {
+    title: "West, District 3",
+  },
+  {
+    title: "South, District 4",
+  },
+];
